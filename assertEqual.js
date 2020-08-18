ab672ef598f62e6f156ea3de6b7c52ed1170cfd5
lisafaggiolo <lisa.faggiolo@live.ca>
2020-08-18 02:06:50 +0000

    change in concatenation

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
  
 const assertEqual = (actual, expected) => {
-  const errorMsg = '🤕 Assertion Failed: ' + actual + ' !== ' + expected;
-  const passMsg = '😀 Assertion Passed: ' + actual + ' === ' + expected;
+  const errorMsg = `🤕 Assertion Failed: ${actual} !== ${expected}`;
+  const passMsg = `😀 Assertion Passed: ${actual} === ${expected}`;
   
   if (actual === expected) {
     console.log(passMsg);
