2dbe0287cd626e8ab109f41ad17af38bb79839e5
lisafaggiolo <lisa.faggiolo@live.ca>
2020-08-19 02:14:00 +0000

    code asserting if 2 arrays are equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,35 @@
+ 
+const assertEqual = (actual, expected) => {
+  const errorMsg = `🤕 Assertion Failed: ${actual} !== ${expected}`;
+  const passMsg = `😀 Assertion Passed: ${actual} === ${expected}`;
+  
+  if (actual === expected) {
+    console.log(passMsg);
+  } else {
+    console.log(errorMsg);
+  }
+   
+};
+
+
+
+const eqArrays = (actual, expected) => {
+ 
+  // if length is not equal
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+  // comparing each element of array
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] === expected[i]) {
+        return true;
+      
+      } else {
+        return false;
+      }
+    }
+  }
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
