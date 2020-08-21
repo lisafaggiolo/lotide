2c77a766c19e520f30a97162d3a48ee74c29831b
lisafaggiolo <lisa.faggiolo@live.ca>
2020-08-21 02:39:05 +0000

    code that iterates over an array until an element matches the givent predicate. returns any elements that were there prior.

---
+++ takeUntil.js
@@ -0,0 +1,22 @@
+const takeUntil = (array, callback) => {
+  // x < 0
+  let goodIndex = 0;
+  
+  array.map((value, index) => {
+    if (callback(value)) {
+      goodIndex += index;
+    }
+  });
+
+  return array.slice(0, goodIndex);
+  
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
