038fe1aef73c1c4782585cdee5452b6e127761fa
lisafaggiolo <lisa.faggiolo@live.ca>
2020-08-22 18:31:59 +0000

    code that founds an object key with a given value

---
+++ findKey.js
@@ -0,0 +1,35 @@
+
+
+
+const assertEqual = (actual, expected) => {
+  const errorMsg = `🤕 Assertion Failed: ${actual} !== ${expected}`;
+  const passMsg = `😀 Assertion Passed: ${actual} === ${expected}`;
+  
+  if (actual === expected) {
+    console.log(passMsg);
+  } else {
+    console.log(errorMsg);
+  }
+   
+};
+
+
+
+const findKey = (starsObj, callback) => {
+
+  for (let x in starsObj) {
+    if (callback(starsObj[x])) {
+      return x;
+    }
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma') // => "noma"
+
